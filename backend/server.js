ccdbe625672607e578a80eca50966f37b2f11c45
isaacSemakula <isaaclennon18@gmail.com>
2026-04-18 00:30:21 +0100

    Fix backend server syntax

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-backend/server.jsconst express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
